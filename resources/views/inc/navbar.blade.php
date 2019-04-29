bb6e28ceb41dbcbad3a811202d88736089e52caa
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-04-29 01:30:40 -0500

    Added a new view when the post title is clicked linked by his id

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -12,7 +12,10 @@
                     <a class="nav-link" href="/about">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " href="/services">Services</a>
+                    <a class="nav-link" href="/services">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/posts">Blog</a>
                 </li>
             </ul>
             <form class="form-inline mt-2 mt-md-0">
